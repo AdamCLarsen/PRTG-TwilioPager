adcb803bf9e0b78645adf88107d1b9b6a6ba4758
Adam Larsen <Adam@collabip.com>
2014-04-21 10:53:27 -0500

    Initial Checkin

---
+++ README.md.txt
@@ -0,0 +1,12 @@
+Quick Start
+-----------
+Read up on PRTG execute a batch file as notification. [http://www.paessler.com/knowledgebase/en/topic/2543-how-can-i-execute-a-batch-file-as-notification]
+
+Make sure .Net Framework 4.5 is install on your core server.
+
+Enable "Execute Program" in your notification, select "Prtg.pager.twilio" from the program files, if it doesn't show up then you likely don't have it in the correct folder.
+First parameter is the phone number to send the message to, the the rest is the message that's send
+
+Example parameters: 5555551234 %group %device %shortname: %status %down (%message)
+
+You will also need to edit the "Prtg.Pager.Twilio.exe.config" file, to enter your Twilio API keys and source phone number you want to use.
---
+++ src/Prtg.Pager.Twilio/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Twilio;
+
+namespace Prtg.Pager.Twilio
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var trc = new TwilioRestClient(ConfigurationManager.AppSettings["TwilioAccountSid"],
+										   ConfigurationManager.AppSettings["TwilioAuthToken"]);
+			var fromNumber = ConfigurationManager.AppSettings["TwilioNumber"];
+			var to = CheckAndFormatPhone(args[0]);
+			var message = string.Join(" ", args.Skip(1));
+
+			Console.WriteLine("From: {0}\r\nTo: {1}\r\nMessage: \"{2}\"", fromNumber ?? "NULL", to ?? "NULL", message ?? "NULL");
+
+			if (string.IsNullOrEmpty(fromNumber) || string.IsNullOrEmpty(to) || string.IsNullOrEmpty(message))
+			{
+				Console.WriteLine("Skipping, argument missing.");
+				return;
+			}
+
+			var messageResponse = trc.SendSmsMessage(fromNumber, to, message);
+			if (messageResponse == null)
+			{
+				throw new ApplicationException("Didn't get a response from SMS provider.");
+			}
+
+			Console.WriteLine("Message sent: {0} - {1}", messageResponse.Status, messageResponse.Sid);
+		}
+
+		public static string CheckAndFormatPhone(string phone)
+		{
+			var checkAndFormatPhoneRegex = new Regex(@"^(\+?1[ -.]?)?\(?([0-9]{3})\)?[ -.]?([0-9]{3})[ -.]?([0-9]{4})$", RegexOptions.Compiled);
+			if (string.IsNullOrEmpty(phone))
+			{
+				throw new InvalidDataException("Phone number is not in a valid format.");
+			}
+
+			var match = checkAndFormatPhoneRegex.Match(phone);
+			if (!match.Success)
+			{
+				throw new InvalidDataException("Phone number is not in a valid format.");
+			}
+
+			return string.Join(string.Empty, "+1", match.Groups[2], match.Groups[3], match.Groups[4]);
+		}
+	}
+}
